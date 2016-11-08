586260b4964616e4bdf4ba5a6530c37a726f2cf5
Gabriel Souza <gabriel.souza@locaweb.com.br>
2016-11-08 19:51:28 -0200

    some changes

--- lib/scit.rb
+++ lib/scit.rb
@@ -1,7 +1,18 @@
+require 'net/http'
 require 'virtus'
 
 module Scit
   def self.call
     puts 'Everything ok'
+    puts download
+  end
+
+  def self.download
+    ::Net::HTTP.get(uri)
+  end
+
+  def self.uri
+    URI('http://shopicruit.myshopify.com/products.json?page=1')
   end
 end
+
